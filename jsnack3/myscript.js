0d0310b3886154e8e6dab030dec89b1532ff7c7c
Vito9009 <yan.vi193@gmail.com>
2021-12-16 21:55:30 +0100

    jsnack3

--- jsnack3/myscript.js
+++ jsnack3/myscript.js
@@ -11,12 +11,25 @@ const evenNumber = []; // variabile numeri pari
 
 const oddNumber = []; // variabile numeri dispari
 
+const resultsContainer = document.getElementById("results-container"); // contenitore nel file HTML in cui verranno inseriti i due div
+
+const greenEven = document.createElement("div"); //creazione div ed aggiunta classe per i numeri pari
+greenEven.classList.add("green");
+
+const redOdd = document.createElement("div"); //creazione div ed aggiunta classe per i numeri pari
+redOdd.classList.add("red");
+
 for (let i = 0; i < myArray.length; i++){ // ciclo for
     if(i %2 == 1){  // se i numeri sono pari
         evenNumber.push(myArray[i]);
+        greenEven.innerHTML += myArray[i];
     }else{ // se i numeri sono dispari
         oddNumber.push(myArray[i]);
+        redOdd.innerHTML += myArray[i];
     }
 }
+
+resultsContainer.append(greenEven); // stampa dei numeri pari nel file HTML
+resultsContainer.append(redOdd); // stampa dei numeri dispari nel file HTML
 
 console.log("pari", evenNumber, "dispari", oddNumber); // mostra in console del gruppo dei numeri pari e del gruppo dei numeri dispari
